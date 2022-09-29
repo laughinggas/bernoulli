483fc2893fc22c3841816f511ccbb5aa70042c95
Eric Rodriguez <ericrboidi@gmail.com>
2022-09-29 14:14:56 +0100

    first two days

--- src/sheet1.lean
+++ src/sheet1.lean
@@ -17,31 +17,58 @@ variables (A : Type*) [comm_ring A] [algebra ℚ A]
 def bernoulli_neg (n : ℕ) : ℚ := (-1)^n * bernoulli' n
 
 -- What does the `simp` tag do?
-@[simp] lemma bernoulli_neg_zero : bernoulli_neg 0 = 1 := sorry
+@[simp] lemma bernoulli_neg_zero : bernoulli_neg 0 = 1 :=
+by rw [bernoulli_neg, pow_zero, bernoulli'_zero, mul_one]
 
 -- Try to solve this with and without `norm_num`
-@[simp] lemma bernoulli_neg_one : bernoulli_neg 1 = -1/2 := sorry
+@[simp] lemma bernoulli_neg_one : bernoulli_neg 1 = -1/2 :=
+by rw [bernoulli_neg, pow_one, bernoulli'_one, mul_div, mul_one]
 
 -- What does the statement say? Write a docstring
 theorem bernoulli_neg_eq_bernoulli'_of_ne_one {n : ℕ} (hn : n ≠ 1) : bernoulli_neg n = bernoulli' n :=
 begin
-  -- Step 1. Might be better to look at this by breaking into cases `n = 0` and `n ≠ 0`
-  -- Try using `by_cases` for this.
-  -- Step 2. Now it might help to further split n into odd and even, how can you do that using `cases`?
-  sorry,
+  rcases eq_or_ne n 0 with rfl | hn₀,
+  { rw [bernoulli_neg_zero, bernoulli'_zero] },
+  cases n.even_or_odd with h h,
+  { rw [bernoulli_neg, (neg_one_pow_eq_one_iff_even (show (-1 : ℚ) ≠ 1, by norm_num)).mpr h,
+        one_mul] },
+  { replace hn := one_lt_iff_ne_zero_and_ne_one.mpr ⟨hn₀, hn⟩,
+    simp_rw [bernoulli_neg, bernoulli'_odd_eq_zero h hn, mul_zero] }
 end
+
+lemma bernoulli'_eq : ∀ n, bernoulli' n = (if n = 1 then 1 else 0) + bernoulli_neg n
+| 0     := by simp
+| 1     := by norm_num
+| (n+2) := by simp [bernoulli_neg_eq_bernoulli'_of_ne_one]
+
+lemma one_sub_half : (1 : ℚ) - 1 / 2 = 1 / 2 := by norm_num
 
 -- Challenge!
 -- What does the statement say? Write a docstring ; how do you get the `∑` symbol?
 @[simp] theorem sum_bernoulli_neg (n : ℕ) :
   ∑ k in range n, (n.choose k : ℚ) * bernoulli_neg k = if n = 1 then 1 else 0 :=
 begin
-  -- Step 1. Break this into cases when n is 0 or nonzero. How is this different from using `by_cases`?
-  -- Step 2. You will be needing cases again.
-  -- Step 3. Find a way to make this a sum upto n. 
-  -- Hint : It might help to prove that ∑_{i = 0}^{n - 1} ((n + 2) choose (i + 2)) * B_{i + 2} = n/2
-  -- Can you rewrite this sum in valid Lean code?
-  -- Step 4. Use a property of sum of bernoulli'
-  -- Step 5. ring : New tactic unlocked! 
-  sorry,
+  cases n,
+  { rw [range_zero, sum_empty, if_neg], rintro ⟨⟩ },
+  cases n,
+  { rw [range_one, sum_singleton, bernoulli_neg_zero, mul_one,
+        choose_zero_right, nat.cast_one, if_pos rfl] },
+  rw [if_neg n.succ_succ_ne_one],
+  set q := n.succ.succ with hq,
+  suffices : ∑ x in range q, (q.choose x : ℚ) * bernoulli' x = q,
+  { have key := sum_sdiff_eq_sub (singleton_subset_iff.mpr $ mem_range.mpr n.one_lt_succ_succ),
+    rw [this, sum_singleton, choose_one_right, bernoulli'_one, ←mul_one_sub, one_sub_half] at key,
+    rw [←sum_sdiff (singleton_subset_iff.mpr $ mem_range.mpr n.one_lt_succ_succ), sum_singleton,
+        choose_one_right, bernoulli_neg_one, neg_div, mul_neg, ←key, ←sub_eq_add_neg, sub_eq_zero],
+    refine sum_congr rfl (λ x hx, _),
+    congr',
+    rw [mem_sdiff, mem_singleton] at hx,
+    exact bernoulli_neg_eq_bernoulli'_of_ne_one hx.2 },
+  apply sum_bernoulli',
 end
+
+/-
+
+`elan` is annoying...
+
+-/
--- src/sheet2.lean
+++ src/sheet2.lean
@@ -1,4 +1,5 @@
 import sheet1
+import data.polynomial.ring_division
 
 open_locale big_operators -- so that we can use `∑` instead of `finset.sum`
 open_locale nat polynomial -- so that we can use `n!`
@@ -11,31 +12,76 @@ open polynomial
 -- taking A to be a commutative ring and a ℚ-algebra
 variables {A : Type*} [comm_ring A] [algebra ℚ A]
 
-/-- The Bernoulli polynomials are defined in terms of the negative Bernoulli numbers as `∑_{i = 0}^{n} (n choose i) B_i X^{n - i}`. -/
-noncomputable def bernoulli_poly (n : ℕ) : ℚ[X] := sorry
+/-- The Bernoulli polynomials are defined in terms of the negative Bernoulli numbers 
+as `∑_{i = 0}^{n} (n choose i) B_i X^{n - i}`. -/
+noncomputable def bernoulli_poly (n : ℕ) : ℚ[X] :=
+  ∑ i in range (n + 1), (↑(n.choose i) * bernoulli_neg i) • X ^ (n - i)
 
 -- Now make an API for your definition
-lemma bernoulli_poly_def (n : ℕ) : bernoulli_poly n = sorry :=
-begin
--- Hint : Some useful lemmas might be `sum_range_reflect` and `sum_congr`
-  sorry
-end
-
+lemma bernoulli_poly_def (n : ℕ) :
+  bernoulli_poly n = ∑ i in range (n + 1), (↑(n.choose i) * bernoulli_neg i) • X ^ (n - i) :=
+rfl
 -- You can also look up and prove some more properties, such as B_0(X) = 1 etc.
 
-@[simp] theorem sum_bernoulli_poly (n : ℕ) :
+@[simp] lemma bernoulli_poly_zero : bernoulli_poly 0 = 1 :=
+by simp [bernoulli_poly_def]
+
+@[simp] lemma bernoulli_poly_eval_zero (n : ℕ) : (bernoulli_poly n).eval 0 = bernoulli_neg n :=
+begin
+  rw [bernoulli_poly_def, ←coeff_zero_eq_eval_zero, finset_sum_coeff],
+  simp_rw [polynomial.coeff_smul, algebra.id.smul_eq_mul],
+  convert_to ∑ x in range (n + 1), (if x = n then ↑(n.choose x) * bernoulli_neg x else 0) = _,
+  { refine sum_congr rfl (λ x hx, _),
+    split_ifs,
+    { simp [h] },
+    replace hx := (le_of_lt_succ $ mem_range.mp hx).lt_of_ne h,
+    rw [polynomial.coeff_X_pow, if_neg (tsub_pos_iff_lt.mpr hx).ne, mul_zero] },
+  simp
+end
+
+@[simp] lemma bernoulli_poly_eval_one (n : ℕ) : (bernoulli_poly n).eval 1 = bernoulli' n :=
+begin
+  simp_rw [bernoulli_poly_def, eval_finset_sum, eval_smul, eval_pow, eval_X, one_pow],
+  simp only [rat.smul_one_eq_coe, rat.cast_eq_id, id.def],
+  rw [sum_range_succ, sum_bernoulli_neg, choose_self, nat.cast_one, one_mul, bernoulli'_eq]
+end
+
+lemma sum_zero {α β} {s : finset α} [add_comm_monoid β] : ∑ x in s, (0 : β) = 0 :=
+sum_eq_zero $ λ x hx, rfl
+
+@[simp] theorem sum_bernoulli_poly_aux (n : ℕ) :
   ∑ k in range (n + 1), ((n + 1).choose k : ℚ) • bernoulli_poly k =
     polynomial.monomial n (n + 1 : ℚ) :=
 begin
--- `simp_rw`: new tactic unlocked!
--- the proof depends on your def, so no hints :( ask me for help!
--- always remember to first have a clear pen and paper proof!
-  sorry
+  ext1 t,
+  rw [finset_sum_coeff, polynomial.coeff_monomial],
+  simp_rw [polynomial.coeff_smul, bernoulli_poly, finset_sum_coeff, polynomial.coeff_smul,
+           polynomial.coeff_X_pow],
+  simp_rw [smul_ite, sum_ite, smul_zero, sum_zero, add_zero, smul_sum, algebra.id.smul_eq_mul,
+           mul_one],
+  split_ifs,
+  { subst h,
+     }
 end
+
+#print odd.add_even
+
+#exit
+
+@[simp] theorem sum_bernoulli_poly (n : ℕ) :
+  ∑ k in range n, (n.choose k : ℚ) • bernoulli_poly k =
+    polynomial.monomial (n - 1) (n : ℚ) :=
+begin
+  cases n,
+  { simp },
+  convert sum_bernoulli_poly_aux n,
+  norm_cast,
+end
+
 
 variable {A}
 
-/-- The theorem that `∑ Bₙ(t)X^n/n!)(e^X-1)=Xe^{tX}`  -/
+/-- The theorem that `∑ Bₙ(t) X^n/n!) (e^X-1) = Xe^{tX}`  -/
 theorem bernoulli_generating_function (t : A) :
   mk (λ n, aeval t ((1 / n! : ℚ) • bernoulli_poly n)) * (exp A - 1) =
     power_series.X * rescale t (exp A) :=
