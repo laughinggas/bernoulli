38cc2f6420e0fe5707a0300eb735700ca48558ec
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2022-09-29 14:12:25 +0100

    changes

--- src/sheet1.lean
+++ src/sheet1.lean
@@ -1,7 +1,6 @@
 import data.polynomial.algebra_map
 import data.nat.choose.cast
 import number_theory.bernoulli
--- where do these files come from?
 
 open_locale big_operators -- so that we can use `∑` instead of `finset.sum`
 open_locale nat polynomial -- so that we can use `n!`
@@ -12,36 +11,58 @@ open polynomial
 -- taking A to be a commutative ring and a ℚ-algebra
 variables (A : Type*) [comm_ring A] [algebra ℚ A]
 
--- Go check the definition of `bernoulli'`!
 /-- The Bernoulli numbers are defined to be `bernoulli'` with a parity sign. -/
 def bernoulli_neg (n : ℕ) : ℚ := (-1)^n * bernoulli' n
 
--- What does the `simp` tag do?
-@[simp] lemma bernoulli_neg_zero : bernoulli_neg 0 = 1 := sorry
+@[simp] lemma bernoulli_neg_zero : bernoulli_neg 0 = 1 := by simp [bernoulli_neg, bernoulli'_zero]
 
--- Try to solve this with and without `norm_num`
-@[simp] lemma bernoulli_neg_one : bernoulli_neg 1 = -1/2 := sorry
+@[simp] lemma bernoulli_neg_one : bernoulli_neg 1 = -1/2 := by { rw [bernoulli_neg, bernoulli'_one], norm_num }
 
--- What does the statement say? Write a docstring
-theorem bernoulli_neg_eq_bernoulli'_of_ne_one {n : ℕ} (hn : n ≠ 1) : bernoulli_neg n = bernoulli' n :=
+lemma nat.one_le_of_ne_zero_and_ne_one {n : ℕ} (h0 : n ≠ 0) (h1 : n ≠ 1) : 1 < n :=
 begin
-  -- Step 1. Might be better to look at this by breaking into cases `n = 0` and `n ≠ 0`
-  -- Try using `by_cases` for this.
-  -- Step 2. Now it might help to further split n into odd and even, how can you do that using `cases`?
-  sorry,
+  cases n,
+  { exfalso,
+    apply h0,
+    refl },
+  contrapose h1,
+  push_neg, 
+  simp [nat.succ_le_iff] at h1,
+  simp [h1]
 end
 
--- Challenge!
--- What does the statement say? Write a docstring ; how do you get the `∑` symbol?
+-- bernoulli_neg n = bernoulli' n for all values of n, n ≠ 1
+theorem bernoulli_neg_eq_bernoulli'_of_ne_one {n : ℕ} (hn : n ≠ 1) : bernoulli_neg n = bernoulli' n :=
+begin
+  rw bernoulli_neg,
+  by_cases h0 : n = 0,
+  { rw [h0, bernoulli'_zero],
+    norm_num },
+  by_cases hn' : odd n,
+  -- odd
+  { rw bernoulli'_odd_eq_zero hn' _,
+    norm_num,
+    exact nat.one_le_of_ne_zero_and_ne_one h0 hn },
+  -- even
+  { have h : (-1 : ℚ) ^ n = 1,
+    { rw [odd_iff_not_even, not_not] at hn',
+      cases hn' with k h,
+      simp [h, ← two_mul, pow_mul] },
+    simp [h] }
+end
+
 @[simp] theorem sum_bernoulli_neg (n : ℕ) :
   ∑ k in range n, (n.choose k : ℚ) * bernoulli_neg k = if n = 1 then 1 else 0 :=
 begin
-  -- Step 1. Break this into cases when n is 0 or nonzero. How is this different from using `by_cases`?
-  -- Step 2. You will be needing cases again.
-  -- Step 3. Find a way to make this a sum upto n. 
-  -- Hint : It might help to prove that ∑_{i = 0}^{n - 1} ((n + 2) choose (i + 2)) * B_{i + 2} = n/2
-  -- Can you rewrite this sum in valid Lean code?
-  -- Step 4. Use a property of sum of bernoulli'
-  -- Step 5. ring : New tactic unlocked! 
-  sorry,
+  -- looked at `sum_bernoulli`
+  cases n, { simp },
+  cases n, { simp },
+  suffices : ∑ i in range n, ↑((n + 2).choose (i + 2)) * bernoulli_neg (i + 2) = n / 2,
+  { simp only [this, sum_range_succ', cast_succ, bernoulli_neg_one, bernoulli_neg_zero, choose_one_right,
+    mul_one, choose_zero_right, cast_zero, if_false, zero_add, succ_succ_ne_one], ring },
+  have f := sum_bernoulli' n.succ.succ,
+  simp_rw [sum_range_succ', bernoulli'_one, choose_one_right, cast_succ, ← eq_sub_iff_add_eq] at f,
+  convert f,
+  { funext x, rw bernoulli_neg_eq_bernoulli'_of_ne_one (succ_ne_zero x ∘ succ.inj) },
+  { simp only [one_div, mul_one, bernoulli'_zero, cast_one, choose_zero_right, add_sub_cancel],
+    ring }
 end
--- src/sheet2.lean
+++ src/sheet2.lean
@@ -12,13 +12,19 @@ open polynomial
 variables {A : Type*} [comm_ring A] [algebra ℚ A]
 
 /-- The Bernoulli polynomials are defined in terms of the negative Bernoulli numbers as `∑_{i = 0}^{n} (n choose i) B_i X^{n - i}`. -/
-noncomputable def bernoulli_poly (n : ℕ) : ℚ[X] := sorry
+noncomputable def bernoulli_poly (n : ℕ) : ℚ[X] := 
+  ∑ i in range (n + 1), polynomial.monomial (n - i) ((bernoulli_neg i) * (n.choose i))
 
--- Now make an API for your definition
-lemma bernoulli_poly_def (n : ℕ) : bernoulli_poly n = sorry :=
+lemma bernoulli_poly_def (n : ℕ) : bernoulli_poly n = ∑ i in range (n + 1), polynomial.monomial i ((bernoulli_neg (n - i)) * (n.choose i)) :=
 begin
--- Hint : Some useful lemmas might be `sum_range_reflect` and `sum_congr`
-  sorry
+  rw ←sum_range_reflect,
+  simp,
+  rw [bernoulli_poly, sum_congr rfl],
+  intros k h,
+  rw nat.choose_symm _,
+  { rw nat.sub_sub_self (mem_range_succ_iff.1 h) },
+  rw mem_range at h,
+  exact lt_succ_iff.mp h
 end
 
 -- You can also look up and prove some more properties, such as B_0(X) = 1 etc.
@@ -30,7 +36,49 @@ begin
 -- `simp_rw`: new tactic unlocked!
 -- the proof depends on your def, so no hints :( ask me for help!
 -- always remember to first have a clear pen and paper proof!
-  sorry
+
+-- not my own proof, based off `sum_bernoulli` in `number_theory.bernoulli_polynomials`
+
+  -- ∑ (k = 0 to n) (n+1) choose k • BPₖ = (n + 1) xⁿ
+  -- sub in definition of bernoulli polynomial `bernoulli_poly_def` to get
+  --    ∑ (k = 0 to n) (n+1) choose k • (∑ (i = 0 to k) k choose i * B⁻(k-i) * xⁱ) = (n + 1) xⁿ
+  -- take scalar multiplication in to inner sum
+  --    ∑ (k = 0 to n) (∑ (i = 0 to k) [(n+1) choose k] • [k choose i] * B⁻(n-i) * xⁱ) = (n + 1) xⁿ
+  -- technical details switching `range` to `Ico`
+  -- change to the different way of summing over `(k,i)` in the range `0 ≤ i ≤ k < n + 1` (switch indices)
+  --    ∑ (i = 0 to n) (∑ (k = i to n) [(n+1) choose k] • [k choose i] * B⁻(n-i) * xⁱ) = (n + 1) xⁿ
+  -- Take back to range (starting at 0)
+  --    ∑ (i = 0 to n) (∑ (k = 0 to n - i) [(n+1) choose (k + i)] • [k choose i] * B⁻(n-i) * xⁱ) = (n + 1) xⁿ
+  simp_rw [bernoulli_poly_def, finset.smul_sum, finset.range_eq_Ico, ←finset.sum_Ico_Ico_comm,
+    finset.sum_Ico_eq_sum_range],
+
+  -- 
+  -- ∑ (i = 0 to n) (∑ (k = 0 to n - i) B⁻(k) * [(k+i) choose i] xⁱ) = (n + 1) xⁿ
+  simp only [add_tsub_cancel_left, tsub_zero, zero_add, linear_map.map_add], -- deleted `cast_succ`
+
+
+  simp_rw [smul_monomial, mul_comm (_root_.bernoulli_neg _) _, smul_eq_mul, ←mul_assoc], -- what is the `_root_.` in `_root_.bernoulli_neg` for?
+  conv_lhs { apply_congr, skip, conv
+    { apply_congr, skip,
+      rw [← nat.cast_mul, choose_mul ((le_tsub_iff_left $ mem_range_le H).1
+        $ mem_range_le H_1) (le.intro rfl), nat.cast_mul, add_comm x x_1, add_tsub_cancel_right,
+        mul_assoc, mul_comm, ←smul_eq_mul, ←smul_monomial] },
+    rw [←sum_smul], },
+  rw [sum_range_succ_comm],
+  simp only [add_right_eq_self, cast_succ, mul_one, cast_one, cast_add, add_tsub_cancel_left,
+    choose_succ_self_right, one_smul, _root_.bernoulli_neg_zero, sum_singleton, zero_add,
+    linear_map.map_add, range_one],
+  apply sum_eq_zero (λ x hx, _),
+  have f : ∀ x ∈ range n, ¬ n + 1 - x = 1,
+  { rintros x H, rw [mem_range] at H,
+    rw [eq_comm],
+    exact ne_of_lt (nat.lt_of_lt_of_le one_lt_two (le_tsub_of_add_le_left (succ_le_succ H))) },
+  rw [sum_bernoulli_neg],
+  have g : (ite (n + 1 - x = 1) (1 : ℚ) 0) = 0,
+  { simp only [ite_eq_right_iff, one_ne_zero],
+    intro h₁,
+    exact (f x hx) h₁, },
+  rw [g, zero_smul],
 end
 
 variable {A}
